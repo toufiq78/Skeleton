922840fc94470903dfc6572d4b3fc1a58cad797e
p2763866 <p2763866@KLIXX104WIN018W>
2024-05-22 18:31:14 +0100

    Practical 7

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public bool Status { get; set; }
+        public DateTime Date { get; set; }
+        public int OrderId { get; set; }
+        public string OrderName { get; set; }
+        public int OrderPrice { get; set; }
+        public float Price { get; set; }
+        public int OrderQuantity { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
---
+++ Testing4/tstOrder.cs
@@ -0,0 +1,96 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Assert.IsNotNull(AnOrder );
+        }
+        [TestMethod]
+        public void StatusPropertyOk() 
+        {
+        clsOrder AnOrder =new clsOrder();
+
+        Boolean TestData = true;
+
+            AnOrder.Status = TestData;
+
+            Assert.AreEqual(AnOrder.Status, TestData);
+        }
+        [TestMethod]
+        public void DatePropertyOk() 
+        { 
+            clsOrder AnOrder = new clsOrder();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            AnOrder.Date = TestData;
+
+            Assert.AreEqual(AnOrder.Date, TestData);
+        }
+        [TestMethod]
+        public void OrderIdPropertyOk() 
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Int32 TestData = 1;
+
+            AnOrder.OrderId = TestData;
+
+            Assert.AreEqual(AnOrder.OrderId, TestData);
+        }
+        [TestMethod]
+        public void OrderNamePropertyOk()
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            string TestData = "shirt";
+
+            AnOrder.OrderName = TestData;
+
+            Assert.AreEqual(AnOrder.OrderName, TestData);
+        }
+        [TestMethod]
+        public void PricePropertyOk() 
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Int32 TestData = 1;
+
+            AnOrder.Price = TestData;
+
+            Assert.AreEqual(AnOrder.Price, TestData);
+        }
+        [TestMethod]
+        public void OrderQuantityPropertyOk() 
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            Int32 TestData = 1;
+
+            AnOrder.OrderQuantity = TestData;
+
+            Assert.AreEqual(AnOrder.OrderQuantity, TestData);
+        }
+        [TestMethod]
+        public void PaymentMethodPropertyOk() 
+        {
+            clsOrder AnOrder = new clsOrder();
+
+            string TestData = "cash";
+
+            AnOrder.PaymentMethod = TestData;
+
+            Assert.AreEqual(AnOrder.PaymentMethod, TestData);
+        }
+
+    }
+}
