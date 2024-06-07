3a672a83e22df8b6f90700d35394623d9e5d2e52
Tahmid-Khan007 <P2790484@my365.dmu.ac.uk>
2024-06-07 05:34:55 +0100

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StockTable]
+(
+	[StockId] INT NOT NULL PRIMARY KEY, 
+    [Manufacturer] CHAR(10) NULL, 
+    [Model] VARCHAR(10) NULL, 
+    [Condition] CHAR(10) NULL, 
+    [Price] INT NULL
+)
